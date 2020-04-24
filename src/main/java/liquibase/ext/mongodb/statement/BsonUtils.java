dade1644df53455a7a2890d64b0d5ef8116e0306
Robert Reeves <r2@datical.com>
2020-04-24 10:36:20 -0500

    change UuidRepresentation to JAVA_LEGACY from STANDARD

--- src/main/java/liquibase/ext/mongodb/statement/BsonUtils.java
+++ src/main/java/liquibase/ext/mongodb/statement/BsonUtils.java
@@ -43,7 +43,7 @@ public final class BsonUtils {
 
     public static final DocumentCodec DOCUMENT_CODEC =
         new DocumentCodec(fromProviders(
-            new UuidCodecProvider(UuidRepresentation.STANDARD),
+            new UuidCodecProvider(UuidRepresentation.JAVA_LEGACY),
             new ValueCodecProvider(),
             new BsonValueCodecProvider(),
             new DocumentCodecProvider(),
